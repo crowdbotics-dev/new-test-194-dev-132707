6677e67ddd8ac7da2d67c492c33949ee1b2f06c9
Crowdbotics <engineering@crowdbotics.com>
2024-01-26 14:14:13 +0000

    Update store.

--- store/newtestdevAPI/api.js
+++ store/newtestdevAPI/api.js
@@ -14,6 +14,24 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return newtestdevAPI.post(`/api/v1/signup/`, payload)
 }
+function api_v1_t1_list(payload) {
+  return newtestdevAPI.get(`/api/v1/t1/`)
+}
+function api_v1_t1_create(payload) {
+  return newtestdevAPI.post(`/api/v1/t1/`, payload)
+}
+function api_v1_t1_retrieve(payload) {
+  return newtestdevAPI.get(`/api/v1/t1/${payload.id}/`)
+}
+function api_v1_t1_update(payload) {
+  return newtestdevAPI.put(`/api/v1/t1/${payload.id}/`, payload)
+}
+function api_v1_t1_partial_update(payload) {
+  return newtestdevAPI.patch(`/api/v1/t1/${payload.id}/`, payload)
+}
+function api_v1_t1_destroy(payload) {
+  return newtestdevAPI.delete(`/api/v1/t1/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
   return newtestdevAPI.post(`/rest-auth/login/`, payload)
 }
@@ -51,6 +69,12 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  api_v1_t1_list,
+  api_v1_t1_create,
+  api_v1_t1_retrieve,
+  api_v1_t1_update,
+  api_v1_t1_partial_update,
+  api_v1_t1_destroy,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
